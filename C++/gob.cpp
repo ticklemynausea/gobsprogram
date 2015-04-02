31a612432ccbd231e4d22579350e505eed11d65e
Ian Rust <ian@getcruise.com>
2015-04-01 23:16:38 -0700

    Add C++ implementation

---
+++ C++/gob.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main( int argc , char * argv[])
+{
+    cout << "Gob's Program Y/N?" << endl;
+    cout << "?";
+    
+    string michael;
+
+    cin >> michael;
+
+    if ( michael == "Y" )
+    {
+        while ( true )
+        {
+            cout << "Penus ";
+        }
+    }
+}
