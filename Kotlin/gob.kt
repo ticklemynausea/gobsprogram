a7bfd2d924010dc90430c2d8ef28a3cffe86f08d
João Oliveira <hello@jxs.pt>
2016-03-05 02:59:56 +0000

    Gob's Program in Kotlin

---
+++ Kotlin/gob.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("gobs program?")
+    val input = readLine()
+    if (input == "y") {
+        while (true) {
+            Thread.sleep(100)
+            print("penus")
+        }
+    }
+}
